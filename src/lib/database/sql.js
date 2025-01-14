3c684c83711cad7921d57948e754f930fea132c0
crucess <ignaciocrucescabello@gmail.com>
2025-01-14 20:13:01 +0100

    Fix

--- src/lib/database/sql.js
+++ src/lib/database/sql.js
@@ -1,31 +1,38 @@
 const mysql = require('mysql2');
-const { mysqlDB } = require('../../../config.json');
-var pool = mysql.createPool(mysqlDB);
+require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+
+// Configuración de la base de datos desde .env
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+});
 
 module.exports = {
-    query: function () {
-        var sql_args = [];
-        var args = [];
-        for (var i = 0; i < arguments.length; i++) {
-            args.push(arguments[i]);
+  query: function () {
+    const sql_args = [];
+    const args = Array.from(arguments);
+    const callback = args[args.length - 1];
+
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        console.error(err);
+        return callback(err);
+      }
+
+      if (args.length > 2) {
+        sql_args.push(...args[1]);
+      }
+
+      connection.query(args[0], sql_args, function (err, results) {
+        connection.release();
+        if (err) {
+          console.error(err);
+          return callback(err);
         }
-        var callback = args[args.length - 1];
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            }
-            if (args.length > 2) {
-                sql_args = args[1];
-            }
-            connection.query(args[0], sql_args, function (err, results) {
-                connection.release();
-                if (err) {
-                    console.log(err);
-                    return callback(err);
-                }
-                callback(null, results);
-            });
-        });
-    }
+        callback(null, results);
+      });
+    });
+  },
 };
